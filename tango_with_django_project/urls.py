a503768b204d30a78d69d5cb82482129dfcc270f
“Firstname <3036338k@student.gla.ac.uk>
2026-02-03 18:22:04 +0000

    Initial commit: Tango with Django project

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
@@ -23,3 +26,6 @@ urlpatterns = [
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
